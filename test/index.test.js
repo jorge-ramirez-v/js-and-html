973da1775825d036640b452de12d906f98ff0bb2
Jorge Ramírez V <mpt.jorge@yahoo.com>
2018-02-03 11:18:19 -0600

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+const expect = require('chai').expect
+
+const arr = ['Jorge', 'Daniel', 'Felipe', 'Mitzi', 'Edwin', 'Andrea']
+describe('suite', function(){
+    it('should have a length of 6', function(){
+        expect(arr).to.have.lengthOf(6)
+    })
+    it('should have strings', function(){
+        expect(arr).to.be.a('string')
+    })
+    it('Andrea should be the last element', function(){
+        expect(arr[arr.length-1]).to.equal('Edwin')
+    })
+    })
+    it('Andrea should be in the fifth position')
+        it('Jorge should be the first element', function(){
+        expect(0).to.equal('Andrea')
+    })
+    it('Andrea should be the last element', function(){
+        expect(arr[arr.length-1]).to.equal('Edwin')
+    })
+    it('The read element should be')
+    it('The inserted element should be')
+})
